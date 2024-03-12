5b8a7184fb95634d3f015246aa50d202ce04a512
ants-1 <anthonysalisbury98@outlook.com>
2024-03-12 18:51:14 +0000

    Add form.jade and basic messaging functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+// Middleware to parse request body
+router.use(express.urlencoded({ extended: true }));
+
+router.get('/', (req, res) => {
+  res.render('index', { title: "Mini Messageboard", messages: messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form', { title: "New Message Form", messages: messages });
+});
+
+router.post('/new', (req, res) => {
+  const { author, message } = req.body;
+  messages.push({ text: message, user: author, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
